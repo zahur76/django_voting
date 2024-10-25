ff64ce52bfe9cec4fde8b65b0d154dbc3936a314
zahur <zahurmeerun@hotmail.com>
2024-10-25 11:33:46 +0400

    add: drj spectacular module for API documentation

--- backend/voting_app/settings.py
+++ backend/voting_app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -80,6 +82,11 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
+}
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 
